c8437644e5e25dab4d7909fa0e5f70846b8efa8e
sawarinekodesu <sawarinekodesu@gmail.com>
2024-02-09 18:09:17 +0300

    nvim

---
+++ config/nvim/init.lua
@@ -0,0 +1,12 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+require('core.configs')
+
+-- Plugins
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.cmp')
+require('plugins.mason')
+require('plugins.telescope')
---
+++ config/nvim/lua/core/plugins.lua
@@ -0,0 +1,39 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    			}
+		},
+	{'nvim-treesitter/nvim-treesitter'},
+	{'neovim/nvim-lspconfig'},
+	{'navarasu/onedark.nvim'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'},
+    {'hrsh7th/nvim-cmp'},
+    {"williamboman/mason.nvim", build = ":MasonUpdate"},
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+                                  branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+})
